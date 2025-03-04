c6e18490b10d03437466f9c5d4c67b6af2bbcde9
JoserraRuizDev <joserraruizdev@gmail.com>
2025-03-04 01:53:36 +0100

    Entregable Sprint 2 Unidad 1 completo

--- py.py
+++ py.py
@@ -1,7 +1,19 @@
-mi_variable = "Economía"
-otra_var = "Ejercicio"
-mi_variab1e = "Alpha"
-exec = True
-mi_variable10 = 6
+dni= "55555555F"
+dni2= "53768462M"
+palabra='TRWAGMYFPDXBNJZSQVHLCKE'
+#print('La letra del DNI es: ', palabra[DNI%23])
 
-print(mi_variable10)
+
+def valid_dni(dni):
+    dni_number = dni[:-1]
+    dni_letter = dni[-1]
+    palabra='TRWAGMYFPDXBNJZSQVHLCKE'
+    is_valid = True
+    if dni_number.__len__() >8 or dni_number.__len__() <7:
+        is_valid = False
+    if dni_letter != palabra[int(dni_number)%23]:
+        is_valid = False
+    return is_valid
+
+print(valid_dni(dni))
+print(valid_dni(dni2))
